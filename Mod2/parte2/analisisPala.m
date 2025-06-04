90a9723184907d5c0e1b601b3c2204ff4a6496b0
SantiColu <santiago.colu@gmail.com>
2025-06-04 13:26:17 -0300

    add individual fems & analisis

---
+++ Mod2/parte2/analisisPala.m
@@ -0,0 +1,24 @@
+close all; clear;
+
+[Mp, Kp, rp] = femPala();
+N = length(rp);
+
+% empotramos la pala
+Mp = Mp(3:end, 3:end);
+Kp = Kp(3:end, 3:end);
+
+Kp_ = Mp\Kp;
+[Vp,Dp] = eig(Kp_, 'vector');
+[Dp_s, dpIdx] = sort(Dp);
+Wp = sqrt(Dp_s)/(2*pi); %frecuencias de los modos en hz
+
+figure(3)
+for h = 1:6
+  subplot(3,2,h)
+  v = [0;Vp((1:2:2*(N-1)),dpIdx(h))];
+  plot(rp, -(1/max(abs(v)))*v, "LineWidth", 2)
+  title(["Modo ", num2str(h) " (" num2str(Wp(h)) "Hz)"])
+
+  grid on;
+end
+
---
+++ Mod2/parte2/analisisTorre.m
@@ -0,0 +1,25 @@
+close all; clear;
+
+[Mt, Kt, xt] = femTorre();
+
+N = length(xt);
+
+% empotramos la torre
+Mt = Mt(3:end, 3:end);
+Kt = Kt(3:end, 3:end);
+
+Kt_ = Mt\Kt;
+[Vt,Dt] = eig(Kt_, 'vector');
+[Dt_s, dtIdx] = sort(Dt);
+Wt = sqrt(Dt_s)/(2*pi); %frecuencias de los modos en hz
+
+figure(3)
+for h = 1:6
+  subplot(3,2,h)
+  v = [0;Vt((1:2:2*(N-1)),dtIdx(h))];
+  plot(xt, -(1/max(abs(v)))*v, "LineWidth", 2)
+  title(["Modo ", num2str(h) " (" num2str(Wt(h)) "Hz)"])
+
+  grid on;
+end
+
